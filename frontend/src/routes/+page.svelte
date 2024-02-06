dcd8b6370809ca49743873747786ef7b2cd071c8
axeliono <69375446+axeliono@users.noreply.github.com>
2024-02-05 22:15:04 -0600

    mobile first -> navbar, hero/about/services;Started on books;TODO: fix sticky navbar, inpage links;

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,12 +1,51 @@
 <script lang="ts">
-	import Navbar from "$lib/Navbar.svelte";
-	import darkModeChecked from "$lib/index";
-
-    const setTheme = () => {
-        return (darkModeChecked ? "dark" : "light");
-        
-    }
+	import Homepage from './Homepage/+page.svelte';
 </script>
-<div data-theme="{setTheme}">
-    <Navbar/>
-</div>
+
+<main class="font-garamond">
+	<div class="drawer drawer-end">
+		<input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+		<div class="drawer-content flex flex-col">
+			<!-- Navbar -->
+			<nav class="w-full navbar bg-base-100 sticky top-0 border-b-2">
+				<div class="flex-1 px-2 mx-2 text-2xl">
+					<a href="#home" class="no-underline">Living In The Bonus</a>
+				</div>
+				<div class="flex-none hidden lg:block">
+					<ul class="menu menu-horizontal">
+						<!-- Navbar menu content here -->
+						<li><a href="#about">About Me</a></li>
+						<li><a href="#books">Books</a></li>
+						<li><a href="#services">Services</a></li>
+					</ul>
+				</div>
+				<div class="flex-none lg:hidden">
+					<label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							class="inline-block w-6 h-6 stroke-current"
+							><path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M4 6h16M4 12h16M4 18h16"
+							/></svg
+						>
+					</label>
+				</div>
+			</nav>
+			<Homepage data />
+		</div>
+		<div class="drawer-side">
+			<label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay" />
+			<ul class="menu p-4 w-80 min-h-full bg-base-200 z-50">
+				<!-- Sidebar content here -->
+				<li><a href="#about">About Me</a></li>
+				<li><a href="#books">Books</a></li>
+				<li><a href="#services">Services</a></li>
+			</ul>
+		</div>
+	</div>
+</main>
---
+++ frontend/src/routes/Homepage/+page.svelte
@@ -0,0 +1,134 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<a id="home" aria-hidden="true" style="visibility: hidden;" />
+<section class="h-dvh w-full text-sky-600">
+	<figure class="flex items-center p-5 flex-col lg:flex-row">
+		<img
+			class="rounded-3xl max-w-full w-5/6 h-1/4 p-2 lg:w-2/4 lg:h-dvh"
+			src="Sharon C. Lopez_files/950b4f224a4d7104566c3d2457ff4a7c.jpg"
+			alt="Sharon C. Lopez"
+		/>
+		<header class="text-center text-3xl px-5 w-full lg:w-2/4">
+			<h1 class="leading-loose font-bold">Living In The Bonus</h1>
+			<br />
+			<p>
+				Sharon C. Lopez, LPC <br />
+				Author, Keynote Speaker
+			</p>
+		</header>
+	</figure>
+</section>
+<a id="about" aria-hidden="true" style="visibility: hidden;" />
+<section
+	class=" text-sky-700 bg-yellow-300 p-5 h-dvh text-center text-3xl w-full flex flex-col lg:flex-row lg:justify-around"
+>
+	<div class="p-5">
+		<h2 class="text-3xl">
+			Sharon declares God's
+			<br />
+			<span class="italic">faithfulness</span>
+			in hidden rooms to public platforms, inspiring others to live in the
+			<br />
+			<span class="italic">bonus</span>
+		</h2>
+	</div>
+	<div>
+		<h4 class="mb-3 text-orange-400">About Sharon C. Lopez</h4>
+		<p class="text-base">
+			Sharon is a licensed professional counselor, author, speaker, and hope-giver. She earned her
+			Masters of Science in Counseling Psychology from Southwestern Assemblies of God University in
+			2011 and has compassionately assisted children, adults, and families in crisis and transition.
+			Prior to this work, Sharon married her high school sweetheart, Mark, and earned her Bachelor
+			of Science in Church Ministries. She and Mark served as youth pastors, worship pastors, and
+			executive pastors in Maryland, New Mexico, and Texas. Together they have four amazing
+			children, Madison, Miquela, Elisha, Elise, and a son-in-love, Chandler. Sharon views
+			individuals as God’s greatest treasure. She is a Jesus-follower, life-speaker, and
+			disciple-maker.
+		</p>
+	</div>
+</section>
+<a id="services" aria-hidden="true" style="visibility: hidden;" />
+<section class="border-b-orange-400 text-3xl p-5 text-sky-600 text-center">
+	<h1 class="text-4xl">Services</h1>
+	<p class="text-sm">
+		Sharon leverages decades of expertise in Christian counseling to offer an array of services for
+		individuals, families and organizations
+	</p>
+	<div class="flex flex-col lg:flex-row flex-wrap">
+		<div class="">
+			<div class="card w-96 shadow-xl border-sky-700 mb-3 border-solid">
+				<figure class="px-10 pt-10">
+					<img
+						src="Sharon C. Lopez_files\1246b2f4e49db8b6d0d1820dadcd3c90.jpg"
+						alt="Microphone in front of audience"
+						class="rounded-xl"
+					/>
+				</figure>
+				<div class="card-body items-center text-center">
+					<h2 class="card-title">Speaking Engagements</h2>
+					<div class="card-actions">
+						<!-- <button class="btn btn-neutral">Book</button> -->
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="card w-96 shadow-xl border-solid border-sky-700 mb-3">
+			<figure class="px-10 pt-10">
+				<img
+					src="Sharon C. Lopez_files/e1716a3e4a1330469c61cfa952dc0ca0.jpg"
+					alt="Happy father carrying smiling daughter on back while holding her hands"
+					class="rounded-xl"
+				/>
+			</figure>
+			<div class="card-body items-center text-center">
+				<h2 class="card-title">Parent Coaching & Co-Parent Coaching</h2>
+				<div class="card-actions">
+					<!-- <button class="btn btn-neutral">Book</button> -->
+				</div>
+			</div>
+		</div>
+		<div class="card w-96 shadow-xl border-solid border-sky-700 mb-3">
+			<figure class="px-10 pt-10">
+				<img
+					src="Sharon C. Lopez_files/f75ab1606355b4e7225519bdf83d0f26.jpg"
+					alt="Microphone in front of audience"
+					class="rounded-xl"
+				/>
+			</figure>
+			<div class="card-body items-center text-center">
+				<h2 class="card-title">Consulting & Counseling</h2>
+				<div class="card-actions">
+					<!-- <button class="btn btn-neutral">Book</button> -->
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<a id="books" aria-hidden="true" style="visibility: hidden;" />
+<section class="pt-10">
+	<h2>Books</h2>
+	<p>Sharon is a bold author who seeks to glorify God through her writing</p>
+	<div>
+		<figure class="px-10 pt-10">
+			<img class="w-3/4" src="Sharon C. Lopez_files/1c810f0f0d7b23b98a9495de8f9be0d8.jpg" />
+		</figure>
+		<h1>Dinosaur Dance</h1>
+		<p>
+			Written as an acrostic poem, this children's book empowers young readers to chase their dreams
+			and live fully as the unique person God created them to be
+		</p>
+	</div>
+	<div>
+		<img />
+		<h1>Hope Over Encouragement: A 31 Day Devotional</h1>
+		<p>
+			Authored in partnership with her two sisters, Sharon explores how you can experience hope in
+			seasons of discouragement
+		</p>
+	</div>
+</section>
